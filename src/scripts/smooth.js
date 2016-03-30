edcdf3077e4dfdbec2d178ba0282129d520b9cda
william-abboud <william_y_abboud@abv.bg>
2016-03-30 01:06:36 +0100

    Started translate functionality Added transitioning

--- src/scripts/smooth.js
+++ src/scripts/smooth.js
@@ -7,13 +7,43 @@
 
 const smooth = function smooth(selector) {
   // DOM References
-  let slider = document.querySelector(selector); // TODO: Support for multiple sliders
+  let slider = document.querySelector(selector); // TODO: Add support for multiple sliders
+  let dimensions = slider.getBoundingClientRect();
   let slides;
+
+  // TODO: Add support for vertical, parameter handling
+  const _translate = function(direction) {
+    const offset = (direction === 'backwards') ? (-dimensions.width) : dimensions.width;
+    let [existingOffset] = /-?\d+/g.exec(slider.style.transform) || [0];
+
+    existingOffset = parseInt(existingOffset);
+    slider.style.transform = `translateX(${existingOffset + offset}px)`;
+  };
+
+  const slideForward = function slideForward() {
+
+  };
+
+  const slideBackwards = function slideBackwards() {
+    _translate('backwards');
+  };
+
+  const arrangeSlides = function arrangeSlides() {
+
+  };
 
   // API
   const api = {
-    slide(direction) {
+    slide(direction= 'forward') {
+      if (direction !== 'forward' && direction !== 'backwards') {
+        return; // TODO: Specify what to return + handle other parameters
+      }
 
+      if (direction === 'forward') {
+
+      } else {
+        slideBackwards();
+      }
     }
   };
 
@@ -22,4 +52,6 @@ const smooth = function smooth(selector) {
   }
 
   slides = slider.children;
+
+  return api;
 };
